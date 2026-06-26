c09302de71a4d039e43a30bf052bcec2de3b9d7e
Francyne <francyne_nayara@hotmail.com>
2026-06-26 13:36:42 -0300

    configg inicial do bot

--- config.bot.py
+++ config.bot.py
@@ -1,8 +1,14 @@
-HORA = 19
-MINUTO = 0
+from datetime import datetime
 
+# Data e hora do envio (horário de Brasília)
+DATA_ENVIO = datetime(2026, 6, 26, 19, 0, 0)
+
+# ID do grupo
 GRUPO_ID = -5562907846
 
+# Mensagem
 MENSAGEM = """
-Sua mensagem aqui...
+🔥 teste teste.
+
+Pode ter várias linhas.
 """
